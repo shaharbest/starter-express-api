1aafd6fb6d605ce4454f8a96efea0c337be045b4
Shahar Best <bestshahar9@gmail.com>
2024-01-30 21:33:35 +0200

    id fix

--- models/Product.js
+++ models/Product.js
@@ -3,6 +3,12 @@ const mongoose = require('mongoose');
 const productSchema = new mongoose.Schema({
 	name: String,
 	price: Number,
+}, {
+	toJSON: {
+		virtuals: true,
+		versionKey: false,
+		
+	}
 });
 
 module.exports = mongoose.model('product', productSchema);

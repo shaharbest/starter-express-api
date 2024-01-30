eeecd5225f88a58ad4f12192a2745f38b7e12c39
Shahar Best <bestshahar9@gmail.com>
2024-01-30 20:21:36 +0200

    cors

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 require('dotenv').config();
 const express = require('express')
+const cors = require('cors')
 const app = express()
 const db = require('./db');
 
 db.connect();
 
 app.use(express.json());
+app.use(cors({ origin: '*' }));
 
 app.get('/ping', (req, res) => {
     res.send('pong')
